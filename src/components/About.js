0645083c79a47d929e48805c741cf43793cfadd8
Devin Mullin <devin.mullin90@gmail.com>
2021-09-21 16:04:40 -0500

    done

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>
+      About me
+        <p>
+          I'm a freakin developer!
+        </p>
+        <img src={image} alt="I made this" />
+      </h2>
+  </div>;
 }
 
 export default About;
